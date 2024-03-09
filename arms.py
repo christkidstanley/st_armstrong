10a873050e197da27b30a64956a0f34f0d46785a
christkidstanley <christopherstanley152@gmail.com>
2024-03-09 11:24:15 +0530

    defined the function arms()

--- arms.py
+++ arms.py
@@ -1,1 +1,12 @@
-def arms():
+def arms(a):
+    rem = 0
+    res = 0
+    temp = a
+    while a != 0:
+        rem = int(a % 10)
+        res = (res*10)+rem
+        a = int(a/10)
+    if res == temp :
+        return 0
+    else:
+        return 1
