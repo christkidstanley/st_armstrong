37b9db7b47fe5b1f0d539f5bc0becb04623717d5
christkidstanley <christopherstanley152@gmail.com>
2024-03-09 11:32:36 +0530

    Added a input and evaluting section

--- arms.py
+++ arms.py
@@ -4,9 +4,16 @@ def arms(a):
     temp = a
     while a != 0:
         rem = int(a % 10)
-        res = (res*10)+rem
-        a = int(a/10)
-    if res == temp :
+        res = (res * 10) + rem
+        a = int(a / 10)
+    if res == temp:
         return 0
     else:
         return 1
+
+
+a = int(input("Enter a number a"))
+if arms(a) == 0:
+    print("The given number is an armstrong number")
+else:
+    print("The given number is not an armstrong number")
